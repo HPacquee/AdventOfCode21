e7dbf2d632080513610415c17153953851c63ccc
Haroun Pacquée <haroun.pacquee@vrt.be>
2021-12-05 01:40:06 +0100

    This year the example input for both parts seems to be the same

--- src/test/java/be/hpacquee/aoc2021/TestPuzzle01.java
+++ src/test/java/be/hpacquee/aoc2021/TestPuzzle01.java
@@ -5,37 +5,29 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestPuzzle01 extends BasePuzzleTest {
+
+    private final String puzzleInput = """
+            199
+            200
+            208
+            210
+            200
+            207
+            240
+            269
+            260
+            263
+            """;
+
     @Test
     public void testPart1Example1() {
-        var puzzle = new Puzzle01("""
-                199
-                200
-                208
-                210
-                200
-                207
-                240
-                269
-                260
-                263
-                """);
+        var puzzle = new Puzzle01(puzzleInput);
         assertEquals(puzzle.solvePart1(), "7");
     }
 
     @Test
     public void testPart1Example2() {
-        var puzzle = new Puzzle01("""
-                199
-                200
-                208
-                210
-                200
-                207
-                240
-                269
-                260
-                263
-                """);
+        var puzzle = new Puzzle01(puzzleInput);
         assertEquals(puzzle.solvePart2(), "5");
     }
 
--- src/test/java/be/hpacquee/aoc2021/TestPuzzle03.java
+++ src/test/java/be/hpacquee/aoc2021/TestPuzzle03.java
@@ -5,41 +5,31 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestPuzzle03 extends BasePuzzleTest {
+
+    private final String puzzleInput = """
+            00100
+            11110
+            10110
+            10111
+            10101
+            01111
+            00111
+            11100
+            10000
+            11001
+            00010
+            01010
+            """;
+
     @Test
     public void testPart1Example1() {
-        var puzzle = new Puzzle03("""
-                00100
-                11110
-                10110
-                10111
-                10101
-                01111
-                00111
-                11100
-                10000
-                11001
-                00010
-                01010
-                """);
+        var puzzle = new Puzzle03(puzzleInput);
         assertEquals(puzzle.solvePart1(), "198");
     }
 
     @Test
     public void testPart2Example1() {
-        var puzzle = new Puzzle03("""
-                00100
-                11110
-                10110
-                10111
-                10101
-                01111
-                00111
-                11100
-                10000
-                11001
-                00010
-                01010
-                """);
+        var puzzle = new Puzzle03(puzzleInput);
         assertEquals(puzzle.solvePart2(), "230");
     }
 
